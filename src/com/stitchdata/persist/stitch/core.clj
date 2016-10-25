a2563b52555c71fff0e9cd27ddd91268b1a6ee9c
Christopher Merrick <cmerrick@rjmetrics.com>
2016-10-24 23:31:20 -0400

    parse headers and do one-record-at-a-time

--- src/com/stitchdata/persist/stitch/core.clj
+++ src/com/stitchdata/persist/stitch/core.clj
@@ -3,6 +3,15 @@
             [com.stitchdata.client.core :as sc])
   (:gen-class))
 
+(def records-persisted (atom 0))
+
+(defn parse-headers
+  [in-reader]
+  (let [protocol (first (line-seq in-reader))
+        headers (doall (take-while (partial not= "--") (line-seq in-reader)))]
+    (when (not= protocol "stitchstream/0.1")
+      (throw (ex-info (str "Unsupported protocol: " protocol) {})))))
+
 (defn -main [& args]
   (let [stitch-token (or (System/getenv "STITCH_TOKEN")
                          (throw (ex-info "STITCH_TOKEN not set" {})))
@@ -13,19 +22,24 @@
     (binding [*out* *err*]
       (println "Persisting to Stitch"))
     (try
-      (with-open [stitch-client (sc/client {::sc/client-id (Integer/parseInt stitch-client-id)
+      (with-open [in-reader     (java.io.BufferedReader. *in*)
+                  stitch-client (sc/client {::sc/client-id (Integer/parseInt stitch-client-id)
                                             ::sc/token stitch-token
                                             ::sc/namespace stitch-namespace})]
-        (doseq [ln (line-seq (java.io.BufferedReader. *in*))]
+        (parse-headers in-reader)
+        (doseq [ln (line-seq in-reader)]
           (let [parsed (parse-string ln)]
             (condp = (get parsed "type")
 
-              "RECORDS" (doseq [record (get parsed "records")]
-                          (sc/push stitch-client {::sc/action ::sc/upsert
-                                                  ::sc/sequence (System/currentTimeMillis)
-                                                  ::sc/table-name (get parsed "stream")
-                                                  ::sc/key-names (get parsed "key_fields")
-                                                  ::sc/data record}))
+              "RECORD" (do
+                         (sc/push stitch-client {::sc/action ::sc/upsert
+                                                 ::sc/sequence (System/currentTimeMillis)
+                                                 ::sc/table-name (get parsed "stream")
+                                                 ::sc/key-names (get parsed "key_fields")
+                                                 ::sc/data (get parsed "record")})
+                         (swap! records-persisted inc))
 
               "BOOKMARK" (binding [*out* *err*]
-                           (println "Bookmark:" (generate-string (get parsed "value")))))))))))
+                           (println "Bookmark:" (generate-string (get parsed "value")))))))))
+    (binding [*out* *err*]
+      (println "Persisted" @records-persisted "records"))))
