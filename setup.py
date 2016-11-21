62372d42de402353abcb676af1df6020805792bb
Christopher Merrick <chris@stitchdata.com>
2016-11-21 11:52:23 -0500

    prep for release

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup, find_packages
 import os.path
 
-setup(name="persist-stitch",
-      version="0.1.0",
-      description="A persister for the Stitch API",
-      author="Stitch",
-      url="https://github.com/stitchstreams/persist-stitch",
+setup(name='persist-stitch',
+      version='0.2.0',
+      description='A persister for the Stitch API',
+      author='Stitch',
+      url='https://github.com/stitchstreams/persist-stitch',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
       packages=find_packages(),
       scripts=['bin/persist-stitch'],
-      install_requires=["stitchclient"])
+      install_requires=['stitchclient'])
