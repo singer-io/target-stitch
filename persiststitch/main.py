bd4ef19cd595dcb3bc452b1271b79447faa62694
Christopher Merrick <chris@stitchdata.com>
2016-12-01 15:52:41 -0500

    bump to 0.2.0: run streamer as a child process

--- persiststitch/main.py
+++ persiststitch/main.py
@@ -1,9 +1,13 @@
 import argparse
 import io
+import logging
+import os
 import sys
 import time
 import json
 import jsonschema
+import asyncio
+import asyncio.subprocess
 
 from datetime import datetime
 from dateutil import tz
@@ -11,65 +15,10 @@ from strict_rfc3339 import rfc3339_to_timestamp
 from jsonschema import Draft4Validator, validators
 from stitchclient.client import Client
 
+MAX_LINE_SIZE=4*1024*1024 # 4mb limit
+logger = logging.getLogger()
 schema_cache = {}
-
-def parse_headers():
-    headers = {'version': sys.stdin.readline().strip().lower()}
-
-    for line in sys.stdin:
-        if line.strip() == '--':
-            break
-        (k,v) = line.split(':', 1)
-        headers[k.strip().lower()] = v.strip().lower()
-
-    return headers
-
-
-def parse_key_fields(stream_name):
-    if stream_name in schema_cache:
-        return [k for (k,v) in schema_cache[stream_name]['properties'].items() if 'key' in v and v['key'] == True]
-    else:
-        return []
-
-
-def parse_record(full_record):
-    stream_name = full_record['stream']
-    schema = schema_cache[stream_name] if stream_name in schema_cache else {}
-    o = full_record['record']
-    v = extend_with_default(Draft4Validator)
-    v(schema, format_checker=jsonschema.FormatChecker()).validate(o)
-    return o
-
-
-def from_jsonline(args):
-    global schema_cache
-    last_bookmark = None
-    def persist_bookmark(vals):
-        if last_bookmark is not None:
-            print(json.dumps(last_bookmark), flush=True)
-
-    with Client(args.cid, args.token, callback_function=persist_bookmark) as stitch:
-        while True:
-            line = sys.stdin.readline() # `for line in stdin` won't play nice in all situations
-            if line == '':
-                break
-            o = json.loads(line)
-
-            if o['type'] == 'RECORD':
-                key_fields = parse_key_fields(o['stream'])
-                parsed_record = parse_record(o)
-                stitch.push({'action': 'upsert',
-                             'table_name': o['stream'],
-                             'key_names': key_fields,
-                             'sequence': int(time.time() * 1000),
-                             'data': parsed_record}, last_bookmark)
-            elif o['type'] == 'BOOKMARK':
-                last_bookmark = o['value']
-            elif o['type'] == 'SCHEMA':
-                schema_cache[o['stream']] = o['schema']
-            else:
-                pass
-
+last_bookmark = None
     
 def extend_with_default(validator_class):
     validate_properties = validator_class.VALIDATORS["properties"]
@@ -92,34 +41,110 @@ def extend_with_default(validator_class):
     )
 
 
-def main(args):
-    parser = argparse.ArgumentParser(prog="Stitch Persister")
-    parser.add_argument('-T', '--token', required=True, help='Stitch API token')
-    parser.add_argument('-C', '--cid', required=True, type=int, help='Stitch Client ID')
-    args = parser.parse_args(args)
+def parse_headers(raw_headers):
+    headers = {'version': raw_headers.pop(0).strip().lower()}
 
-    headers = parse_headers()
+    for line in raw_headers:
+        (k,v) = line.split(':', 1)
+        headers[k.strip().lower()] = v.strip().lower()
 
-    if headers['content-type'] == "jsonline":
-        from_jsonline(args)
+    return headers
+
+
+def parse_key_fields(stream_name):
+    if stream_name in schema_cache:
+        return [k for (k,v) in schema_cache[stream_name]['properties'].items() if 'key' in v and v['key'] == True]
     else:
-        raise RuntimeError("Unsupported content-type: {}".format(headers['content-type']))
+        return []
 
-if __name__ == '__main__':
-    schema = {
-        'type':'object',
-        'properties':{
-            'id':{'type':'int', 'key':True},
-            'ts':{'type':'string','format':'date-time'},
-            'max_int':{'type': 'integer'},
-            'max_float':{'type': 'number'},
-            'obj':{'type': 'object',
-                   'properties': {
-                       'nested_date': {'type':'string', 'format':'date-time'}
-                   }
-               }
-    },'required':['ts', 'max_int', 'max_float']}
-    obj = json.loads('{"ts":"2016-01-01T00:00:00Z", "max_int":9223372036854775807, "max_float": 9223372036854775807.9223372036854775807, "obj":{"nested_date":"2017-01-01T00:00:00Z"}}')
+    
+def parse_record(full_record):
+    stream_name = full_record['stream']
+    schema = schema_cache[stream_name] if stream_name in schema_cache else {}
+    o = full_record['record']
     v = extend_with_default(Draft4Validator)
-    v(schema, format_checker=jsonschema.FormatChecker()).validate(obj)
-    print(obj)
+    v(schema, format_checker=jsonschema.FormatChecker()).validate(o)
+    return o
+
+
+def print_bookmark(vals):
+    logger.info('Persisted {} records to Stitch'.format(len(vals)))
+    if last_bookmark is not None:
+        print(json.dumps(last_bookmark), flush=True)
+
+        
+def from_jsonline(stitchclient, line):
+    global schema_cache, last_bookmark
+    o = json.loads(line)
+    if o['type'] == 'RECORD':
+        key_fields = parse_key_fields(o['stream'])
+        parsed_record = parse_record(o)
+        stitchclient.push({'action': 'upsert',
+                           'table_name': o['stream'],
+                           'key_names': key_fields,
+                           'sequence': int(time.time() * 1000),
+                           'data': parsed_record}, last_bookmark)
+    elif o['type'] == 'BOOKMARK':
+        last_bookmark = o['value']
+    elif o['type'] == 'SCHEMA':
+        schema_cache[o['stream']] = o['schema']
+    else:
+        pass
+
+async def run_subprocess(stitchclient, args):
+    create = asyncio.create_subprocess_exec(
+        *args,
+        stdout=asyncio.subprocess.PIPE,
+        limit=MAX_LINE_SIZE
+    )
+    proc = await create
+    logger.info('Subprocess started {}'.format(proc.pid))
+    
+    raw_headers = []
+    
+    while True:
+        line = await proc.stdout.readline() 
+        if line.decode('utf-8').strip() == '--':
+            break
+        raw_headers.append(line.decode('utf-8'))
+
+    headers = parse_headers(raw_headers)
+
+    if headers['content-type'] == 'jsonline':
+        persist_fn = from_jsonline
+    else:
+        raise RuntimeError('Unsupported content-type: {}'.format(headers['content-type']))
+
+    while True:
+        line = await proc.stdout.readline()
+        line = line.decode('utf-8')
+        if line == '' or line is None:
+            break
+        persist_fn(stitchclient, line)
+            
+    logger.info('Waiting for process {} to complete'.format(proc.pid))
+    await proc.wait()
+
+    return_code = proc.returncode
+    logger.info('Subprocess {} returned code {}'.format(proc.pid, return_code))
+
+    return return_code
+
+
+def main(args):
+    with Client(
+            int(os.environ['STITCH_CLIENT_ID']),
+            os.environ['STITCH_TOKEN'],
+            callback_function=print_bookmark
+    ) as stitchclient:
+        event_loop = asyncio.get_event_loop()
+        try:
+            return_code = event_loop.run_until_complete(
+                run_subprocess(stitchclient, args)
+            )
+        finally:
+            event_loop.close()
+
+            
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='persist-stitch',
-      version='0.1.0',
+      version='0.2.0',
       description='A persister for the Stitch API',
       author='Stitch',
       url='https://github.com/stitchstreams/persist-stitch',
