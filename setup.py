97d6b1bdda0648096397bd15df9916ce7c8ffc22
Christopher Merrick <chris@stitchdata.com>
2016-12-12 21:58:29 -0500

    specify stitch client version requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='persist-stitch',
-      version='0.2.2',
+      version='0.2.3',
       description='A persister for the Stitch API',
       author='Stitch',
       url='https://github.com/stitchstreams/persist-stitch',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       packages=find_packages(),
       scripts=['bin/persist-stitch'],
-      install_requires=['stitchclient', 'jsonschema', 'strict-rfc3339'])
+      install_requires=['stitchclient>=0.4.1', 'jsonschema', 'strict-rfc3339'])
