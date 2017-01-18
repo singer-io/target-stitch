d1d7a692bee11645ddf3e1bc30fdca793272284f
Andrew Madonna <amadonna@rjmetrics.com>
2017-01-18 15:19:42 -0500

    Add dry run and state file flags

--- persiststitch/main.py
+++ persiststitch/main.py
@@ -1,20 +1,26 @@
 import argparse
+import atexit
 import logging
 import os
+import copy
+import io
 import sys
 import time
 import json
-import jsonschema
-
 from datetime import datetime
 from dateutil import tz
+
 from strict_rfc3339 import rfc3339_to_timestamp
-from jsonschema import Draft4Validator, validators
+
+from jsonschema import Draft4Validator, validators, FormatChecker
 from stitchclient.client import Client
 
 logger = logging.getLogger()
 schema_cache = {}
+dry_run = False
+state_file = None
 last_state = None
+row_count = 0
 
 def configure_logging(level=logging.DEBUG):
     global logger
@@ -24,6 +30,7 @@ def configure_logging(level=logging.DEBUG):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
+
 
 def extend_with_default(validator_class):
     validate_properties = validator_class.VALIDATORS["properties"]
@@ -57,29 +64,45 @@ def parse_key_fields(stream_name):
 def parse_record(full_record):
     stream_name = full_record['stream']
     schema = schema_cache[stream_name] if stream_name in schema_cache else {}
-    o = full_record['record']
+    o = copy.deepcopy(full_record['record'])
     v = extend_with_default(Draft4Validator)
-    v(schema, format_checker=jsonschema.FormatChecker()).validate(o)
+    v(schema, format_checker=FormatChecker()).validate(o)
     return o
 
 
 def push_state(vals):
-    logger.info('Persisted {} records to Stitch'.format(len(vals)))
+    global state_file, last_state
+
+    if vals is not None:
+        logger.info('Persisted {} records to Stitch'.format(len(vals)))
+
     if last_state is not None:
-        print("I would persist this state " + json.dumps(last_state))
+        if state_file is not None:
+            state = json.dumps(last_state)
+            logger.info("Persisting state to file " + state)
+            state_file.seek(0)
+            state_file.write(state)
+        else:
+            logger.info("I would persist this state " + json.dumps(last_state))
 
         
 def persist_line(stitchclient, line):
-    global schema_cache, last_state
+    global dry_run, schema_cache, last_state, state_file, row_count
     o = json.loads(line)
     if o['type'] == 'RECORD':
         key_fields = parse_key_fields(o['stream'])
         parsed_record = parse_record(o)
-        stitchclient.push({'action': 'upsert',
-                           'table_name': o['stream'],
-                           'key_names': key_fields,
-                           'sequence': int(time.time() * 1000),
-                           'data': parsed_record}, last_state)
+        row_count += 1
+        if state_file is not None and row_count % 100 == 0:
+            push_state(None)
+        if dry_run:
+            logger.info('Dry Run - Would persist: ' + json.dumps(o))
+        else:
+            stitchclient.push({'action': 'upsert',
+                               'table_name': o['stream'],
+                               'key_names': key_fields,
+                               'sequence': int(time.time() * 1000),
+                               'data': parsed_record}, last_state)
     elif o['type'] == 'STATE':
         last_state = o['value']
     elif o['type'] == 'SCHEMA':
@@ -88,21 +111,47 @@ def persist_line(stitchclient, line):
         pass
 
 
-def main(args):
+def main():
+    global dry_run, state_file, last_state
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dry',
+                        '--dry-run',
+                        help='Dry run - Do not push data to Stitch',
+                        dest='dry_run',
+                        action='store_true')
+    parser.add_argument('--state-file',
+                        help='Store state to a local file')
+
+    args = parser.parse_args()
+
+    if args.dry_run:
+        dry_run = True
+
+    if args.state_file:
+        state_file = open(args.state_file, 'w')
+
     configure_logging()
     with Client(
             int(os.environ['STITCH_CLIENT_ID']),
             os.environ['STITCH_TOKEN'],
             callback_function=push_state
     ) as stitchclient:
+        input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
         while True:
-            line = proc.stdout.readline()
-            line = line.decode('utf-8')
+            line = input_stream.readline()
             if line == '' or line is None:
                 break
             persist_line(stitchclient, line)
 
+    def exit_handler():
+        if state_file is not None:
+            state_file.seek(0)
+            state_file.write(json.dumps(last_state))
+            state_file.truncate()
+            state_file.close()
+
+    atexit.register(exit_handler)
 
             
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
