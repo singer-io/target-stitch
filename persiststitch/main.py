403aa03b1f2b4fa01f27f83d99e042a92e4b7996
Mike DeLaurentis <miked@stitchdata.com>
2017-01-24 13:55:45 -0500

    Load config from file

--- persiststitch/main.py
+++ persiststitch/main.py
@@ -15,7 +15,6 @@ from jsonschema import Draft4Validator, validators, FormatChecker
 from stitchclient.client import Client
 
 logger = logging.getLogger()
-schema_cache = {}
 
 
 class DryRunClient(object):
@@ -46,13 +45,13 @@ class DryRunClient(object):
         self.flush()
 
 
-def configure_logging(level=logging.DEBUG):
+# TODO: Maybe we'll put this in a shared lib
+def configure_logging(level=logging.DEBUG,foo={}):
     global logger
     logger.setLevel(level)
     ch = logging.StreamHandler()
     ch.setLevel(level)
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
@@ -84,20 +83,20 @@ def extend_with_default(validator_class):
     )
 
 
-def parse_key_fields(stream_name):
-    if (stream_name in schema_cache and
-            'properties' in schema_cache[stream_name]):
-        return [k for (k, v) in schema_cache[stream_name]['properties'].items()
+def parse_key_fields(stream_name, schemas):
+    if (stream_name in schemas and
+            'properties' in schemas[stream_name]):
+        return [k for (k, v) in schemas[stream_name]['properties'].items()
                 if 'key' in v and v['key'] is True]
     else:
         return []
 
 
-def parse_record(full_record):
+def parse_record(full_record, schemas):
     try:
         stream_name = full_record['stream']
-        if stream_name in schema_cache:
-            schema = schema_cache[stream_name]
+        if stream_name in schemas:
+            schema = schemas[stream_name]
         else:
             schema = {}
         o = copy.deepcopy(full_record['record'])
@@ -108,75 +107,109 @@ def parse_record(full_record):
         raise Exception("Error parsing record {}".format(full_record))
 
 
-# TODO: Get review on state saving part
 def push_state(states):
+    """Called with the list of states associated with the messages that we
+just persisted to the gate. These states will often be None.
+
+    """
     logger.info('Persisted batch of {} records to Stitch'.format(len(states)))
     for state in states:
         if state is not None:
             logger.debug('Emitting state {}'.format(state))
-            print(state)
-
+            sys.stdout.write("{}\n".format(state))
+            sys.stdout.flush()
+            
 
 def persist_lines(stitchclient, lines):
-    global schema_cache
+    """Takes a client and a stream and persists all the records to the gate,
+printing the state to stdout after each batch."""
     state = None
+    schemas = {}
     for line in lines:
         o = json.loads(line)
         if o['type'] == 'RECORD':
             message = {'action': 'upsert',
                        'table_name': o['stream'],
-                       'key_names': parse_key_fields(o['stream']),
+                       'key_names': parse_key_fields(o['stream'], schemas),
                        'sequence': int(time.time() * 1000),
-                       'data': parse_record(o)}
+                       'data': parse_record(o, schemas)}
             stitchclient.push(message, state)
             state = None
         elif o['type'] == 'STATE':
             logger.debug('Setting state to {}'.format(o['value']))
             state = o['value']
         elif o['type'] == 'SCHEMA':
-            schema_cache[o['stream']] = o['schema']
+            schemas[o['stream']] = o['schema']
         else:
             raise Exception("Unknown message type {} in message {}"
                             .format(o['type'], o))
     return state
 
 
-def stitch_client(dry_run):
-    if dry_run:
-        return DryRunClient(
-            callback_function=push_state)
-    else:
-        return Client(
-            int(os.environ['STITCH_CLIENT_ID']),
-            os.environ['STITCH_TOKEN'],
-            callback_function=push_state)
-
-
-def main():
-    global dry_run
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d',
-                        '--dry-run',
-                        help='Dry run - Do not push data to Stitch',
-                        dest='dry_run',
-                        action='store_true')
-    parser.add_argument('--state-file',
-                        help='Store state to a local file')
-
-    args = parser.parse_args()
-
+def stitch_client(args):
+    """Returns an instance of StitchClient or DryRunClient"""
     if args.dry_run:
-        dry_run = True
+        return DryRunClient(callback_function=push_state)
+    else:
+        with open(args.config) as input:
+            config = json.load(input)
+        missing_fields = []
 
-    configure_logging()
+        if 'client_id' in config:
+            client_id = config['client_id']
+        else:
+            missing_fields.append('client_id')
 
+        if 'token' in config:
+            token = config['token']
+        else:
+            missing_fields.append('token')
+        
+        if len(missing_fields) > 0:
+            raise Exception('Configuration is missing required fields: {}'
+                            .format(missing_fields))
+        return Client(client_id, token, callback_function=push_state)
+
+
+def do_sync(args):
     input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
     state = None
-    with stitch_client(dry_run) as client:
+    with stitch_client(args) as client:
         state = persist_lines(client, input)
     if state is not None:
         logger.debug('Emitting final state {}'.format(state))
         print(state)
+    
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers()
+    
+    parser_sync = subparsers.add_parser('sync')
+    parser_sync.set_defaults(func=do_sync)
+
+    for subparser in [parser_sync]:
+        subparser.add_argument('-c', '--config', help='Config file', required=True)
+    parser_sync.add_argument('-n',
+                             '--dry-run',
+                             help='Dry run - Do not push data to Stitch',
+                             action='store_true')
+    
+    args = parser.parse_args()
+    configure_logging()
+
+    if 'func' in args:
+        args.func(args)
+    else:
+        parser.print_help()
+        exit(1)
+
+    args = parser.parse_args()
+
+    configure_logging()
+
 
 
 if __name__ == '__main__':
