4ff71bb46ed41f057358ecec1d4c530a6305e3da
Mike DeLaurentis <miked@stitchdata.com>
2017-01-26 11:31:38 -0500

    Version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='persist-stitch',
-      version='0.2.3',
+      version='0.3.0',
       description='A persister for the Stitch API',
       author='Stitch',
       url='https://github.com/stitchstreams/persist-stitch',
