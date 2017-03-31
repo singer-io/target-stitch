d126bae07156d3a8634d552438a608569ccc7efb
Christopher Merrick <chris@stitchdata.com>
2017-03-31 06:45:49 -0400

    release 0.7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='0.7.2',
+      version='0.7.3',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
