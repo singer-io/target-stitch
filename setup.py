d300c0c7ea749960e1fb97e566ab77d1d7b71c05
Mike DeLaurentis <miked@stitchdata.com>
2017-09-19 14:46:16 -0400

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='0.11.0.alpha.4',
+      version='1.0.0',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
