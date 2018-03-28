a3fbf32a8600f9238e6b3af05d8ada3a5122be77
matt-zach-s <matthew@stitchdata.com>
2018-03-28 11:20:54 -0400

    update error to clarify the max size of a record

--- target_stitch/__init__.py
+++ target_stitch/__init__.py
@@ -289,7 +289,7 @@ def serialize(messages, schema, key_names, bookmark_names, max_bytes):
 
     if len(messages) <= 1:
         raise BatchTooLargeException(
-            "A single record is larger than batch size limit of {} Mb".format(
+            "A single record is larger than the Stitch API limit of {} Mb".format(
                 max_bytes // 1000000))
 
     pivot = len(messages) // 2
