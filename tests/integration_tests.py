c01b684d48d4f309cc88c647336e0f1f4e30538e
Paul Santa Clara <psantaclara@rjmetrics.com>
2019-07-03 13:29:58 -0400

    removing defunct tests

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -19,49 +19,6 @@ def load_sample_lines(filename):
     with open('tests/' + filename) as fp:
         return [line for line in fp]
 
-def token():
-    token = os.getenv('TARGET_STITCH_TEST_TOKEN')
-    if not token:
-        raise Exception('Integration tests require TARGET_STITCH_TEST_TOKEN environment variable to be set')
-    return token
-
-class TestRecordWithNullKeyProperty(unittest.TestCase):
-    def setUp(self):
-        handler = StitchHandler(token(),
-                                DEFAULT_STITCH_URL,
-                                target_stitch.DEFAULT_MAX_BATCH_BYTES,
-                                target_stitch.DEFAULT_MAX_BATCH_RECORDS)
-        out = io.StringIO()
-        self.target_stitch = target_stitch.TargetStitch(
-            [handler], out, 4000000, 20000, 100000)
-
-    def test(self):
-        queue = load_sample_lines('record_missing_key_property.json')
-        pattern = ('Error persisting data for table '
-                   '"test_record_missing_key_property": '
-                   'Record is missing key property id')
-        with self.assertRaisesRegex(TargetStitchException, pattern):
-            self.target_stitch.consume(queue)
-
-class TestNoToken(unittest.TestCase):
-
-    def setUp(self):
-        token = None
-        handler = StitchHandler(token,
-                                DEFAULT_STITCH_URL,
-                                target_stitch.DEFAULT_MAX_BATCH_BYTES,
-                                target_stitch.DEFAULT_MAX_BATCH_RECORDS)
-        out = io.StringIO()
-        self.target_stitch = target_stitch.TargetStitch(
-            [handler], out, 4000000, 20000, 100000)
-
-    def test(self):
-        queue = load_sample_lines('record_missing_key_property.json')
-        pattern = 'Not Authorized'
-        with self.assertRaisesRegex(TargetStitchException, pattern):
-            self.target_stitch.consume(queue)
-
-
 class FakePost:
     def __init__(self, requests_sent, makeFakeResponse):
         self.requests_sent = requests_sent
@@ -81,7 +38,6 @@ class FakeSession:
     def post(self, url, *, data, **kwargs):
         self.requests_sent = self.requests_sent + 1
         return FakePost(self.requests_sent, self.makeFakeResponse)
-
 
 class AsyncPushToGate(unittest.TestCase):
     def setUp(self):
