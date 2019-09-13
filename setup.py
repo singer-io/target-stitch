2451689ba58a47fc021f19340dee1e2c86a42886
Paul Santa Clara <paul@stitchdata.com>
2019-09-13 13:13:00 -0400

    Bump target version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='2.0.3',
+      version='2.0.4',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
