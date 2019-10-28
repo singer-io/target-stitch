284849257d0ede1e6f85d0c5cacee26d77533e47
Paul Santa Clara <paul@stitchdata.com>
2019-10-28 15:06:55 -0400

    refactoring activate_version test for better failure reporting

--- tests/activate_version_tests.py
+++ tests/activate_version_tests.py
@@ -37,22 +37,38 @@ class FakeSession:
 class ActivateVersion(unittest.TestCase):
     def fake_flush_states(self, state_writer, future):
         self.flushed_state_count = self.flushed_state_count + 1
+
         if self.flushed_state_count == 1:
             #2nd request has not begun because it contains an ActivateVersion and must wait for 1 to complete
-            self.assertEqual(1, len(target_stitch.PENDING_REQUESTS), "ActivateVersion request should not have been issues until 1st request completed")
-            self.assertEqual(future, target_stitch.PENDING_REQUESTS[0][0])
-            self.assertEqual({'bookmarks': {'chicken_stream': {'id': 1}}}, target_stitch.PENDING_REQUESTS[0][1])
+            if len(target_stitch.PENDING_REQUESTS) != 1:
+                self.first_flush_error = "ActivateVersion request should not have been issues until 1st request completed: wrong pending request count for first flush"
+
+            if future != target_stitch.PENDING_REQUESTS[0][0]:
+                self.first_flush_error = "ActivateVersion request should not have been issues until 1st request completed: received wrong future for first flush"
+
+            if  target_stitch.PENDING_REQUESTS[0][1] != {'bookmarks': {'chicken_stream': {'id': 1}}}:
+                self.first_flush_error = "ActivateVersion request should not have been issues until 1st request completed: wrong state for first flush"
+
         elif self.flushed_state_count == 2:
-            self.assertEqual(100, len(target_stitch.PENDING_REQUESTS))
-            self.assertEqual(future, target_stitch.PENDING_REQUESTS[0][0])
-            self.assertEqual(None, target_stitch.PENDING_REQUESTS[0][1])
+            if len(target_stitch.PENDING_REQUESTS) != 1:
+                self.second_flush_error = "ActivateVersion request should not have been issues until 1st request completed: wrong pending request count for second flush"
+
+            if future != target_stitch.PENDING_REQUESTS[0][0]:
+                self.second_flush_error = "ActivateVersion request should not have been issues until 1st request completed: wrong future for second flush"
+
+            if target_stitch.PENDING_REQUESTS[0][1] is not None:
+                self.second_flush_error = "ActivateVersion request should not have been issues until 1st request completed: wrong state for second flush"
+
         else:
             raise Exception('flushed state should only have been called twice')
 
         self.og_flush_states(state_writer, future)
 
+
     def setUp(self):
         token = None
+        self.first_flush_error = None
+        self.second_flush_error = None
 
         handler = StitchHandler(token,
                                 DEFAULT_STITCH_URL,
@@ -75,6 +91,9 @@ class ActivateVersion(unittest.TestCase):
 
     def test_activate_version_finishes_pending_requests(self):
         target_stitch.OUR_SESSION = FakeSession(mock_out_of_order_all_200)
+        #request 2 would ordinarily complete first because the mock_out_of_order_all_200, but because
+        #request 2 contains an ACTIVATE_VERSION, it will not even be sent until request 1 completes
+
         self.queue.append(json.dumps({"type": "RECORD", "stream": "chicken_stream", "version":1, "record": {"id": 1, "name": "Mike"}}))
         self.queue.append(json.dumps({"type":"STATE", "value":{"bookmarks":{"chicken_stream":{"id": 1 }}}}))
         #will flush here after 2 records
@@ -85,9 +104,8 @@ class ActivateVersion(unittest.TestCase):
 
         self.target_stitch.consume(self.queue)
         finish_requests()
-
-        #request 2 would ordinarily complete first because the mock_out_of_order_all_200, but because
-        #request 2 contains an ACTIVATE_VERSION, it will not even be sent until request 1 completes
+        self.assertEqual(self.first_flush_error, None, self.first_flush_error)
+        self.assertEqual(self.second_flush_error, None, self.second_flush_error)
 
 
 if __name__== "__main__":
