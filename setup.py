ae0950a435e8f5f8131f536dd4b5b799b8cfc578
Paul Santa Clara <paul@stitchdata.com>
2019-10-29 09:41:54 -0400

    bumping to 2.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='2.0.7',
+      version='2.0.8',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
