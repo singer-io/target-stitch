86b2730629ba751920ff6fe2a0faa3fa312b5c87
Anne Maiale Mead <anne@stitchdata.com>
2019-11-13 16:14:36 +0000

    bump version to 3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='2.0.8',
+      version='3.0.0',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
