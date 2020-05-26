c73202a7a641c5d3ed0e35ebf50ed271054f7eaf
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2020-05-26 13:35:01 -0400

    Fix/use nanosecond time for sequence numbers (#90)
    * Add failing test and signature change for implementation
    
    * Generate sequence numbers using nanosecond precision and modulo
    
    * Remove unused nanoseconds parameter
    
    * Fix parameters in mixed test
    
    * Clarify test comment

--- target_stitch/__init__.py
+++ target_stitch/__init__.py
@@ -47,7 +47,8 @@ StreamMeta = namedtuple('StreamMeta', ['schema', 'key_properties', 'bookmark_pro
 BIGBATCH_MAX_BATCH_BYTES = 20000000
 DEFAULT_MAX_BATCH_BYTES = 4000000
 DEFAULT_MAX_BATCH_RECORDS = 20000
-SEQUENCE_MULTIPLIER = 1000
+MILLISECOND_SEQUENCE_MULTIPLIER = 1000
+NANOSECOND_SEQUENCE_MULTIPLIER = 1000000
 
 # This is our singleton aiohttp session
 OUR_SESSION = None
@@ -422,16 +423,28 @@ class ValidatingHandler: # pylint: disable=too-few-public-methods
             state_writer.flush()
 
 def generate_sequence(message_num, max_records):
-    '''Generates a unique sequence number based on the current time millis
-       with a zero-padded message number based on the magnitude of max_records.'''
-    sequence_base = str(int(time.time() * SEQUENCE_MULTIPLIER))
+    '''
+    Generates a unique sequence number based on the current time in nanoseconds
+    with a zero-padded message number based on the index of the record within the
+    magnitude of max_records.
+
+    COMPATIBILITY:
+    Maintains a historical width of 19 characters (with default `max_records`), in order
+    to not overflow downstream processes that depend on the width of this number.
+
+    Because of this requirement, `message_num` is modulo the difference between nanos
+    and millis to maintain 19 characters.
+    '''
+    nanosecond_sequence_base = str(int(time.time() * NANOSECOND_SEQUENCE_MULTIPLIER))
+    modulo = NANOSECOND_SEQUENCE_MULTIPLIER / MILLISECOND_SEQUENCE_MULTIPLIER
+    zfill_width_mod = len(str(NANOSECOND_SEQUENCE_MULTIPLIER)) - len(str(MILLISECOND_SEQUENCE_MULTIPLIER))
 
     # add an extra order of magnitude to account for the fact that we can
     # actually accept more than the max record count
-    fill = len(str(10 * max_records))
-    sequence_suffix = str(message_num).zfill(fill)
+    fill = len(str(10 * max_records)) - zfill_width_mod
+    sequence_suffix = str(int(message_num % modulo)).zfill(fill)
 
-    return int(sequence_base + sequence_suffix)
+    return int(nanosecond_sequence_base + sequence_suffix)
 
 def serialize(messages, schema, key_names, bookmark_names, max_bytes, max_records):
     '''Produces request bodies for Stitch.
