ef661c662639d8952283ba09de6a59d65029807b
Kyle Allan <kyle@stitchdata.com>
2020-08-17 19:56:19 +0000

    bump to 3.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='3.0.3',
+      version='3.1.0',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
