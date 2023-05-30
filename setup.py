e20a177f4053ae22acf4e018b46ed550c05910f7
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:26 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-stitch',
-      version='3.2.1',
+      version='3.2.2',
       description='Singer.io target for the Stitch API',
       author='Stitch',
       url='https://singer.io',
